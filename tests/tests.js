05eaba216ee1c894fd9c85f062f4aa4f459847a7
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2016-02-20 11:06:28 +0300

    Add manual tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,6 +1,6 @@
 exports.defineAutoTests = function() {
     describe('plugin', function() {
-        it('should be exposed as plugins.appPreferences object', function() {
+        it('should be exposed as cordova.plugins.MyAwesomePlugin object', function() {
             expect(cordova.plugins.MyAwesomePlugin).toBeDefined();
             expect(cordova.require('my-awesome-plugin.MyAwesomePlugin'))
                 .toBe(cordova.plugins.MyAwesomePlugin);
@@ -15,6 +15,24 @@ exports.defineAutoTests = function() {
     });
 };
 
-exports.defineManualTests = function(contentEl, createActionButton) {
-    // To be done
+exports.defineManualTests = function(content, createActionButton) {
+
+    var show_alert_test =
+        '<h3>Press "Show alert" button to display a message box</h3>' +
+        '<div id="show_alert"></div>' +
+        'Expected result: A message box should appear with message "It works!"';
+
+    content.innerHTML = '<div id="info"></div>' + show_alert_test;
+
+    var log = document.getElementById('info');
+    var logMessage = function (message) {
+        var logLine = document.createElement('div');
+        logLine.innerHTML = message;
+        log.appendChild(logLine);
+    };
+
+    createActionButton('Show alert', function() {
+        log.innerHTML = 'Displaying message';
+        cordova.plugins.MyAwesomePlugin.logCoolMessage('It works!');
+    }, "show_alert");
 };
