09e271c2e91c78ff7622ca56ef826a5c26a80e37
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2016-02-20 10:33:18 +0300

    Add auto tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,18 @@
 exports.defineAutoTests = function() {
-    // To be done
+    describe('plugin', function() {
+        it('should be exposed as plugins.appPreferences object', function() {
+            expect(cordova.plugins.MyAwesomePlugin).toBeDefined();
+            expect(cordova.require('my-awesome-plugin.MyAwesomePlugin'))
+                .toBe(cordova.plugins.MyAwesomePlugin);
+        });
+
+        it('should have corresponding methods defined', function() {
+            ['coolMethod', 'logCoolMessage'].forEach(function(methodName) {
+                expect(cordova.plugins.MyAwesomePlugin[methodName]).toBeDefined();
+                expect(cordova.plugins.MyAwesomePlugin[methodName]).toEqual(jasmine.any(Function));
+            });
+        });
+    });
 };
 
 exports.defineManualTests = function(contentEl, createActionButton) {
